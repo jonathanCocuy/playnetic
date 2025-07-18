975c277d23c5bd3480e4f90eaaacff0a18a0e387
Agudrey <cristianagudelorey1@gmail.com>
2025-07-17 22:03:31 -0500

    style(login): changed login form style

--- front/src/screens/loginScreen/LoginScreen.tsx
+++ front/src/screens/loginScreen/LoginScreen.tsx
@@ -1,25 +1,13 @@
 import GridMotion from '../../components/GridMotion/GridMotion';
 import { items } from './gridItems';
+import Login from '../../components/Login/Login';
 import "./LoginScreen.scss";
 
 function LoginScreen() {
 
   return (
     <section className='loginScreen'>
-      <div className='login'>
-        <h1>Login</h1>
-
-        <input type="email" placeholder='Email'/>
-        <input type="password" placeholder='Password'/>
-
-        <button>
-          Sign up
-          <div className="arrow-wrapper">
-              <div className="arrow"></div>
-
-          </div>
-        </button>
-      </div>
+      <Login />
 
       <GridMotion items={items} />
     </section>
