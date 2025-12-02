f4d514d75bbe7dcb74c35c37a883c19d04865aed
jonathanCocuy <jonathancp145@gmail.com>
2025-12-02 16:43:03 -0500

    create new playnetic version

--- front/src/features/gamePuntuation/types.ts
+++
@@ -1,8 +0,0 @@
-export interface gameState {
-    currentLevel: number; // 1 - 3
-    currentExercise: number | string; // 1 - 5
-    pointsGame: number; // 0 - 150
-    totalPoints: number;
-    gamesCompleted: number;
-    color: "green" | "red" | "normal";
-}
