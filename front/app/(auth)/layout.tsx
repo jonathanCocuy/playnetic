ab40924a9ca779813f5ebe1fc38fd3d24cd21420
jonathanCocuy <jonathancp145@gmail.com>
2025-12-18 17:26:50 -0500

    feat(auth): created structure folders for register and login

--- front/app/(auth)/layout.tsx
+++ front/app/(auth)/layout.tsx
@@ -4,9 +4,18 @@ import PrismaticBackground from "@/components/backgrounds/LiquidEtherBackground"
 
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
     return (
-        <div className="flex flex-col items-center justify-center h-screen">
-            <PrismaticBackground />
-            {children}
+        <div className="relative min-h-screen overflow-hidden font-sans">
+            {/* Background */}
+            <div className="absolute inset-0 -z-10">
+                <PrismaticBackground />
+            </div>
+        
+            {/* Content */}
+            <div className="relative z-10 flex min-h-screen items-center justify-center">
+                <div className="w-full max-w-md px-4">
+                {children}
+                </div>
+            </div>
         </div>
     );
 }
--- front/app/(auth)/login/components/LoginForm.tsx
+++ front/app/(auth)/login/components/LoginForm.tsx
@@ -3,10 +3,10 @@ import { Button } from "@/components/ui/Button";
 
 export default function LoginForm() {
     return (
-        <div className="flex flex-col items-center justify-center h-screen">
+        <div className="flex flex-col items-center justify-center h-screen bg-background-light rounded-lg p-8 gap-4">
             <h3 className="text-2xl font-bold">Iniciar sesión</h3>
             <p className="text-sm text-gray-500">Ingresa tus credenciales para continuar</p>
-            <div className="flex flex-col items-center justify-center">
+            <div className="flex flex-col items-center justify-center gap-4">
                 <Input type="email" placeholder="Email" />
                 <Input type="password" placeholder="Contraseña" />
                 <Button variant="dark">Iniciar sesión</Button>
